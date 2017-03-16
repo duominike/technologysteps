29f7b0ae887953c0cb9a6b9ee39971ce5cef306a
duominike <jimmy882010@163.com>
2017-03-16 23:42:22 +0800

    commit md5

---
+++ src/main/java/com/joker/tecsteps/javatecsteps/util/MD5.java
@@ -0,0 +1,42 @@
+package com.joker.tecsteps.javatecsteps.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.security.MessageDigest;
+
+/**
+ * Created by liwenle on 17-3-8.
+ */
+public class MD5 {
+    public static final String getMD5ByFile(String filePath){
+        String value = "";
+        File file = new File(filePath);
+        if(!file.exists()){
+            return value;
+        }
+        FileInputStream in = null;
+        try {
+            in = new FileInputStream(file);
+            MappedByteBuffer byteBuffer = in.getChannel().map(FileChannel.MapMode.READ_ONLY, 0, file.length());
+            MessageDigest md5 = MessageDigest.getInstance("MD5");
+            md5.update(byteBuffer);
+            BigInteger bi = new BigInteger(1, md5.digest());
+            value = bi.toString(16);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if(null != in) {
+                try {
+                    in.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return value;
+    }
+}
